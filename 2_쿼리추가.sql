5cb49c999e63a1d5faea540cd4ff2184ab1541b0
wlstjd3398 <wlstjd3398@naver.com>
2022-04-12 00:38:31 +0900

    DB설치,MariaDB,DB용어

---
+++ 2_쿼리추가.sql
@@ -0,0 +1,7 @@
+INSERT INTO membertbl(memberid, membername, memberaddress) VALUES ("gorden", "고든", "경기 성남구 분당구");
+
+INSERT INTO producttbl(productName, cost, makeDate, company, amount) VALUES('냉장고',10,'2019-07-01','삼성',17);
+
+INSERT INTO producttbl(productName, cost, makeDate, company, amount) VALUES('컴퓨터',20,'2020-03-01','LG',3);
+
+INSERT INTO producttbl(productName, cost, makeDate, company, amount) VALUES('모니터',5,'2021-09-01','롯데',22);
